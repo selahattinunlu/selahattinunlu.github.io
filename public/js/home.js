738c5f40c7612e9ac98babe97bc96f5e987060a5
Selahattin Ünlü <selahattin.unlu@yandex.com>
2016-02-04 17:48:04 +0200

    fix delay issue for typed.js plugin

--- public/js/home.js
+++ public/js/home.js
@@ -1,10 +1,9 @@
-setTimeout(function() {
    $('.home-hero .description span').typed({
       strings: ["Full Time Developer & Part Time Student", "Full Stack Developer & Youtuber"],
       cursorChar: '_',
       contentType: 'text',
       typeSpeed: 30,
+      startDelay: 1500
    });
-}, 1500);
 
 addEffectAsChaining('.footer-item .body', 'zoomIn');
